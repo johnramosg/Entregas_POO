08a9b252b716c13d6005fd960d7b2f919c0ca3ab
John Ramos <ramos.john@correounivalle.edu.co>
2023-09-26 20:53:16 -0500

    Cree el Archivo PantallaInicio.cpp en Netflix

---
+++ Netflix/PantallaInicio.cpp
@@ -0,0 +1,10 @@
+#include "PantallaInicio.h"
+
+PantallaInicio::PantallaInicio(string _ID, string _nombreUsuario, string _peliculasVistas, string _seriesVistas)
+{
+  string ID,nombreUsuario,peliculasVistas,seriesVistas;
+  ID=_ID;
+  nombreUsuario=_nombreUsuario;
+  peliculasVistas=_peliculasVistas;
+  seriesVistas=_seriesVistas;
+}
